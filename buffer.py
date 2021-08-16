e14681bc4bf93835581819a22773eff80ec0d268
Andy Stewart <lazycat.manatee@gmail.com>
2021-08-16 10:15:51 +0800

    Follow EAF's newest background_color API.

--- buffer.py
+++ buffer.py
@@ -37,8 +37,6 @@ class AppBuffer(Buffer):
     def __init__(self, buffer_id, url, arguments):
         Buffer.__init__(self, buffer_id, url, arguments, False)
 
-        self.background_color = QColor(get_emacs_var("eaf-emacs-theme-background-color"))
-
         self.add_widget(FileTransferWidget(url,
                                            get_emacs_var("eaf-emacs-theme-background-color"),
                                            get_emacs_var("eaf-emacs-theme-foreground-color")))
